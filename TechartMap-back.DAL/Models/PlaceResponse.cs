c6bd7a3ff9b4bfea6d673277678a441f04a5031c
Igor <ejenzolotnik@mail.ru>
2020-08-04 15:18:04 +0300

    add session controller

---
+++ TechartMap-back.DAL/Models/PlaceResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TechartMap_back.DAL.Models
+{ 
+    public class PlaceResponse
+    {
+        public int Number;
+        public bool IsFree;
+        public string PlaceType;
+
+        public PlaceResponse(int number, bool isFree, string placeType)
+        {
+            this.Number = number;
+            this.IsFree = isFree;
+            this.PlaceType = placeType;
+        }
+    }
+}
---
+++ TechartMap-back.DAL/Repository/Interfaces/ISessionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TechartMap_back.DAL.Models;
+
+namespace TechartMap_back.DAL.Repository.Interfaces
+{
+    public interface ISessionRepository
+    {
+        Task<IEnumerable<Session>> GetSessions();
+        Task<Session> GetCurrentSession(int id);
+        Task AddSession(Session session);
+        Task<List<List<PlaceResponse>>> GetHallBySession(int id);
+    }
+}
